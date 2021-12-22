bfcf8b801e797ec22341c4c5dfcdc9cca57708cf
Juan Sebastian Velasquez <jsebastian.va@gmail.com>
2021-12-22 10:12:37 -0500

    Comentar parte 3, y añadir graficos

--- parcial/part3/ejercicioParte3.R
+++ parcial/part3/ejercicioParte3.R
@@ -1,45 +1,73 @@
-install.packages('pracma')
-library('pracma')
+##---------------------------------------------------------------------------
+##---------------------------------------------------------------------------
+##---------------------------------------------------------------------------
+## Creado por Juan Sebastian Velasquez Acevedo
+## Codigo: 1744936-3743
+## Parcial de Estadística - Ejercicio - Parte 3
+## Universidad del Valle - Cali - Colombia -Periodo Nov-Mar
+## Correo: juan.velasquez.acevedo@correounivalle.edu.co
+##---------------------------------------------
+## Last modification: Dic 22
 
 coin<-c('head', 'tail')
 
 #set.seed(546378)# Semilla para repoducibilidad, ya que es una operacion aleatoria
-
-#Crear vector vacio donde guardare posteriormente los resultados de cada lanzamiento
-valuesArray <- character() 
-#Crear vector vacio donde guardare los lanzamientos
-flipsArray <- numeric() 
-#Crear vector vacio donde guardare posteriormente los resultados de cada calculo de frecuencia
-relativeFrequencyVector<- vector()
-
+##---------------------------------------------
+##VARIABLES Y CONSTANTES PARA EXPERIMENTO
+##---------------------------------------------
 acum<-0
 oldFlip<-' '
-relativeFreq<-0
 n<-6
 
+##---------------------------------------------
+##FUNCION EXPERIMENTO QUE REPRESENTA EL PROBLEMA DADO DE PROBABILIDAD
+## PROBABILIDAD DE QUE EN 6 LANZAMIENTOS DE UNA MONEDA, NO ME SALGAN 2 CARAS
+## O 2 SELLOS SEGUIDOS
+## Esta funcion simplemente ayuda a la simulacion, ya que devuelve el acum
+## acum es la cantidad de veces que el experimento fue exitoso, lo podriamos llamar una Frecuencia absoluta
+## void() -> return int = acum
+##---------------------------------------------
 experimento <- function() {
   for(numberFlip in 1:n){
     flip  <- sample(coin,1)
-    if(!(strcmp(oldFlip, ' ')))
+    if(!(strcmp(oldFlip, ' '))) #Solo para la 1era repeticion, para que el acum no me cuente
     {
-      if(!(strcmp(flip, oldFlip))){
+      if(!(strcmp(flip, oldFlip))){ #Compara el lanzamiento actual con el anterior
         acum<-acum+1
       }
     }
-    oldFlip<- flip
+    oldFlip<- flip #Asigna el valor del lanzamiento anterior para el proximo ciclo 
   }
-  relativeFreq<-acum/n
   return(acum)
 }
-experimento()
 
+
+##---------------------------------------------
+##CREO FOR EN DONDE HAGO N SIMULACIONES DEL EXPERIMENTO ANTERIOR
+##---------------------------------------------
+
+##Puede ser cualquier numero de repeticiones
+numeroSimulaciones<-10000
+
+#Crear vector vacio donde guardare los lanzamientos
+numSim <- numeric() 
+#Crear vector vacio donde guardare posteriormente los resultados de cada calculo de frecuencia
+relativeFrequencyVector<- vector()
 
 #For donde recorro de 1 a 1000 y luego asigno valores a mi vectorFlips y mi relativeFrequencyVector
-for(numberFlip in 1:10000){
+#Por cada ciclo ejecuta el experimento y lo divide por el numero actual de simulacion
+#La anterior operacion es lo mismo que la frecuencia relativa en ese momento de la simulacion
+
+for(numberFlip in 1:numeroSimulaciones){
   relativeFrequencyVector<-append(relativeFrequencyVector, experimento()/numberFlip)
-  flipsArray<-append(flipsArray,numberFlip)
+  numSim<-append(numSim,numberFlip)
 }
 
+##---------------------------------------------
+##REPRESENTO TODO EN UN GRAFICO, CON EJE X= NUMERO DE SIMULACIONES
+## Y EJE Y= LAS FRECUENCIAS RELATIVAS POR CADA SIMULACION
+## Grafico demuestra que el valor se acerca a 0.03125 que es el valor dado por el enfoque clásico
+##---------------------------------------------
 #Representar todo con un grafico de dispersion para ver el comportamiento
-plot(x=flipsArray,y=relativeFrequencyVector,xlab = "Number of flips", ylab = "Relative frequency")
+plot(x=numSim,y=relativeFrequencyVector,xlab = "Number of simulations", ylab = "Relative frequency")
 abline(h=0.03125,col="blue",lty=1)
