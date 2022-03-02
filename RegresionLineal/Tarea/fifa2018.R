5e2fa890aeb21e79fa5dd366ecc4653a50be7eaa
Juan Sebastian Velasquez <jsebastian.va@gmail.com>
2022-03-02 01:02:07 -0500

    Add error graphics, changed a little bit the code

--- RegresionLineal/Tarea/fifa2018.R
+++ RegresionLineal/Tarea/fifa2018.R
@@ -21,9 +21,13 @@ modLinealSimple
 #βo: Intercepto de la línea con el eje Y. (Intercept)
 #β1: Pendiente de la línea de regresión.
 #Coeficiente de determinacion : r.squared
+#Sigma^2->varianza del error del modelo
 
 summary(modLinealSimple)
+summary(modLinealSimple)$r.square
+summary(modLinealSimple)$sigma^2
 
+#Grafica
 plot(data$Ball.Possession..,data$Attempts,
      main = "Possession vs Attemps",
      xlab = "Posesión de Balón",
@@ -31,12 +35,15 @@ plot(data$Ball.Possession..,data$Attempts,
      col = "blue")
 abline(modLinealSimple,col='red')
 
+#Errores residuales
+error1 <- residuals(modLinealSimple)
+hist(error1)
 #-------------------------------------------------------------------------
-#Regresion Lineal Simple
+#Regresion Lineal Simple 2
 #Para nuestro modelo lineal simple, tomamos:
-#Posesion de balon (PassAccuracy) : x -> Variable independiente-regresora
+#Posesion de balon (Passes) : x -> Variable independiente-regresora
 #Oportunidades de gol (Ball.Possession) : y -> Variable dependiente-respuesta
-# Modelo Lineal para la posesion de balon y la precision de los pases
+# Modelo Lineal para la posesion de balon y el número de pases exitosos
 modLinealSimple2 = lm(data$Ball.Possession.. ~ data$Passes)
 modLinealSimple2
 
@@ -46,8 +53,21 @@ modLinealSimple2
 #βo: Intercepto de la línea con el eje Y. (Intercept)
 #β1: Pendiente de la línea de regresión.
 #Coeficiente de determinacion : r.squared
+#Sigma^2->varianza del error del modelo
 
 summary(modLinealSimple2)
+summary(modLinealSimple2)$r.square
+summary(modLinealSimple2)$sigma^2
 
-plot(data$Passes,data$Ball.Possession..)
+#Graficas
+plot(data$Passes,data$Ball.Possession..,
+     main = "Passes vs Possession",
+     xlab = "Número de pases exitosos",
+     ylab = "Posesión del balón",
+     col = "blue")
 abline(modLinealSimple2,col='red')
+
+
+#Errores residuales
+error2 <- residuals(modLinealSimple2)
+hist(error2)
